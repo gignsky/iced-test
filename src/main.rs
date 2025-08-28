f36f39a59530d714d1d76aab5359c03efa19039f
Maxwell Rupp <maxwell@giglab.dev>
2025-08-27 21:52:03 -0400

    reset crate to default hello world

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,10 @@
 // ARGS Parser
-use clap::Parser;
-use iced_test::{format_greeting, is_verbose_mode, Args};
+// use clap::Parser;
 
-// // TUI Stuff
-// use color_eyre::Result;
-// use crossterm::event::{self, Event};
-// use ratatui::{DefaultTerminal, Frame};
-
-// Non-TUI Stuff
 fn main() {
-    let args = Args::parse();
-    if is_verbose_mode(&args) {
-        println!("DEBUG {args:?}");
-    }
-    println!("{}", format_greeting(args.name));
+    // let args = Args::parse();
+    // if is_verbose_mode(&args) {
+    //     println!("DEBUG {args:?}");
+    // }
+    println!("hello world");
 }
-
-// // TUI Stuff
-// fn main() -> Result<()> {
-//     color_eyre::install()?;
-//     let terminal = ratatui::init();
-//     let result = run(terminal);
-//     ratatui::restore();
-//     result
-// }
-
-// fn run(mut terminal: DefaultTerminal) -> Result<()> {
-//     loop {
-//         terminal.draw(render)?;
-//         if matches!(event::read()?, Event::Key(_)) {
-//             break Ok(());
-//         }
-//     }
-// }
-
-// fn render(frame: &mut Frame) {
-//     let args = Args::parse();
-//     if args.verbose {
-//         println!("DEBUG {args:?}");
-//     }
-//     let welcome_message = format!(
-//         "Hello {} (from iced-test)!",
-//         args.name.unwrap_or("world".to_string())
-//     );
-//     frame.render_widget(welcome_message, frame.area());
-// }
